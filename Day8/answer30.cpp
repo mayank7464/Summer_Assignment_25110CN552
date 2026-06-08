da5ac4396fca9199d2dc9b175cfa4e3090b6cc05
mayank agarwal <mayankagarwal74558@gmail.com>
2026-06-08 19:48:55 +0530

    Day8 assignment is uploaded

---
+++ Day8/answer30.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of rows or columns :";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        int a=1;
+        for (int j=1;j<=i;j++){
+            cout<<a<<" ";
+            a=a+1;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
